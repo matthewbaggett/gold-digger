7fcdb61740bb2c219b757ff7978d699deb6413e3
Matthew Baggett <matthew@baggett.me>
2017-08-28 19:28:51 +0100

    Code cleanup, change deadzone to 0.2

--- digger.js
+++ digger.js
@@ -1,171 +1,152 @@
-var deadZone = 0.15;
+var deadZone = 0.2;
 var lastMessage;
-  
-  function doConnect()
-  {
+
+function doConnect() {
     websocket = new WebSocket("ws://golddigger:8000");
-    websocket.onopen = function(evt) { onOpen(evt) };
-    websocket.onclose = function(evt) { onClose(evt) };
-    websocket.onmessage = function(evt) { onMessage(evt) };
-    websocket.onerror = function(evt) { onError(evt) };
-  }
+    websocket.onopen = function(evt) {
+        onOpen(evt)
+    };
+    websocket.onclose = function(evt) {
+        onClose(evt)
+    };
+    websocket.onmessage = function(evt) {
+        onMessage(evt)
+    };
+    websocket.onerror = function(evt) {
+        onError(evt)
+    };
+}
 
-  function onOpen(evt)
-  {
+function onOpen(evt) {
     console.log("connected");
+}
 
-  }
-
-  function onClose(evt)
-  {
+function onClose(evt) {
     console.log("disconnected");
-  }
+}
 
-  function onMessage(evt)
-  {
+function onMessage(evt) {
     console.log("response: " + evt.data + '');
-  }
+}
 
-  function onError(evt)
-  {
+function onError(evt) {
     console.log('error: ' + evt.data + '');
+    websocket.close();
+}
 
-	websocket.close();
-  }
-
-  function doSend(message)
-  {
-    if(message != lastMessage){
-        if(websocket.readyState == websocket.CONNECTING)
-        {
+function doSend(message) {
+    if (message != lastMessage) {
+        if (websocket.readyState == websocket.CONNECTING) {
             return false
         }
-	    if(websocket.readyState == websocket.CLOSED || websocket.readyState == websocket.CLOSING){
-		    doConnect();
-	    }
-	    
-        console.log("sent:", message); 
+        if (websocket.readyState == websocket.CLOSED || websocket.readyState == websocket.CLOSING) {
+            doConnect();
+        }
+        console.log("sent:", message);
         websocket.send(message);
         lastMessage = message;
-    }else{
+    } else {
         return false;
     }
-  }
+}
+window.addEventListener("load", init, false);
 
-  window.addEventListener("load", init, false);
+function sendText() {
+    doSend(document.myform.inputtext.value);
+}
 
+function clearText() {
+    document.myform.outputtext.value = "";
+}
 
-   function sendText() {
-		doSend( document.myform.inputtext.value );
-   }
-
-  function clearText() {
-		document.myform.outputtext.value = "";
-   }
-
-   function doDisconnect() {
-		websocket.close();
-   }
-
-
+function doDisconnect() {
+    websocket.close();
+}
 var haveEvents = 'ongamepadconnected' in window;
 var controllers = {};
 
 function connecthandler(e) {
-  addgamepad(e.gamepad);
+    addgamepad(e.gamepad);
 }
 
 function addgamepad(gamepad) {
-  console.log("added gamepad", gamepad);
-  controllers[gamepad.index] = gamepad;
+    console.log("added gamepad", gamepad);
+    controllers[gamepad.index] = gamepad;
 }
 
 function disconnecthandler(e) {
-  removegamepad(e.gamepad);
+    removegamepad(e.gamepad);
 }
 
 function removegamepad(gamepad) {
-  delete controllers[gamepad.index];
+    delete controllers[gamepad.index];
 }
 
 function updateStatus() {
-
-  if (!haveEvents) {
-    scangamepads();
-  }
-
-  var i = 0;
-  var j;
-
-  for (j in controllers) {
-    var controller = controllers[j];
-	var motionMessage;
-	var turretMessage;
-
-    for (i = 0; i < controller.buttons.length; i++) {
-      //console.log("button " + i + ": " + controller.buttons[i]);
+    if (!haveEvents) {
+        scangamepads();
     }
-	var axis = [];
-    for (i = 0; i < controller.axes.length; i++) {
-      //console.log("axis " + i + ": " + controller.axes[i].toFixed(4) + " value = " + ( controller.axes[i] + 1));
-	  axis[i] = controller.axes[i].toFixed(4);
+    var i = 0;
+    var j;
+    for (j in controllers) {
+        var controller = controllers[j];
+        var motionMessage;
+        var turretMessage;
+        for (i = 0; i < controller.buttons.length; i++) {
+            //console.log("button " + i + ": " + controller.buttons[i]);
+        }
+        var axis = [];
+        for (i = 0; i < controller.axes.length; i++) {
+            //console.log("axis " + i + ": " + controller.axes[i].toFixed(4) + " value = " + ( controller.axes[i] + 1));
+            axis[i] = controller.axes[i].toFixed(4);
+        }
+        if (axis[0] > -0.5 && axis[0] < 0.5 || axis[1] > -0.5 && axis[1] < 0.5) {
+            motionMessage = 'stop 0';
+        }
+        if (axis[0] < deadZone * -1) {
+            rate = Math.round(100 * Math.abs(axis[0]));
+            motionMessage = 'left ' + rate.toString();
+        } else if (axis[0] > deadZone) {
+            rate = Math.round(100 * Math.abs(axis[0]));
+            motionMessage = 'right ' + rate.toString();
+        }
+        if (axis[1] < deadZone * -1) {
+            rate = Math.round(100 * Math.abs(axis[1]));
+            motionMessage = 'forward ' + rate.toString();
+        } else if (axis[1] > deadZone) {
+            rate = Math.round(100 * Math.abs(axis[1]));
+            motionMessage = 'backward ' + rate.toString();
+        }
+        if (axis[2] < deadZone && axis[2] > deadZone * -1) {
+            turretMessage = 'stop 0';
+        } else if (axis[2] < deadZone * -1) {
+            rate = Math.round(100 * Math.abs(axis[2]));
+            turretMessage = 'left' + ' ' + rate.toString();
+        } else if (axis[2] > deadZone) {
+            rate = Math.round(100 * Math.abs(axis[2]));
+            turretMessage = 'right' + ' ' + rate.toString();
+        }
+        doSend('track_' + motionMessage + '\n' + 'turret_' + turretMessage);
     }
-
-	if(axis[0] > -0.5 && axis[0] < 0.5 || axis[1] > -0.5 && axis[1] < 0.5){
-      motionMessage = 'stop 0';
-    }
-
-	if(axis[0] < deadZone * -1){
-      rate = Math.round(100 * Math.abs(axis[0]));
-      motionMessage = 'left ' + rate.toString();
-	}else if (axis[0] > deadZone){
-      rate = Math.round(100 * Math.abs(axis[0]));
-      motionMessage = 'right ' + rate.toString();
-	}
-	if(axis[1] < deadZone * -1){
-      rate = Math.round(100 * Math.abs(axis[1]));
-      motionMessage = 'forward ' + rate.toString();
-	}else if (axis[1] > deadZone){
-      rate = Math.round(100 * Math.abs(axis[1]));
-      motionMessage = 'backward ' + rate.toString();
-	}
-	
-	if(axis[2] < deadZone && axis[2] > deadZone * -1){
-	  turretMessage = 'stop 0';
-    }else if(axis[2] < deadZone * -1){
-      rate = Math.round(100 * Math.abs(axis[2]));
-      turretMessage = 'left' + ' ' + rate.toString();
-    }else if(axis[2] > deadZone){
-      rate = Math.round(100 * Math.abs(axis[2]));
-      turretMessage = 'right' + ' ' + rate.toString();
-    }
-
-    doSend('track_' + motionMessage + '\n' + 'turret_' + turretMessage);
-  }
 }
 
 function scangamepads() {
-  var gamepads = navigator.getGamepads ? navigator.getGamepads() : (navigator.webkitGetGamepads ? navigator.webkitGetGamepads() : []);
-  for (var i = 0; i < gamepads.length; i++) {
-    if (gamepads[i]) {
-      if (gamepads[i].index in controllers) {
-        controllers[gamepads[i].index] = gamepads[i];
-      } else {
-        addgamepad(gamepads[i]);
-      }
+    var gamepads = navigator.getGamepads ? navigator.getGamepads() : (navigator.webkitGetGamepads ? navigator.webkitGetGamepads() : []);
+    for (var i = 0; i < gamepads.length; i++) {
+        if (gamepads[i]) {
+            if (gamepads[i].index in controllers) {
+                controllers[gamepads[i].index] = gamepads[i];
+            } else {
+                addgamepad(gamepads[i]);
+            }
+        }
     }
-  }
 }
 
- function init()
-  {
-	doConnect();
+function init() {
+    doConnect();
     setInterval(updateStatus, 100);
     //updateStatus();
-  }
-
-
+}
 window.addEventListener("gamepadconnected", connecthandler);
 window.addEventListener("gamepaddisconnected", disconnecthandler);
-
-
